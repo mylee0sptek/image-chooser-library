d885450a9df7de54460e8b719a583bd0d058ebd2
Moon-Young, Lee <mylee@sptek.co.kr>
2015-11-03 18:44:49 +0900

    선택 타입 추가

--- image-chooser-library/src/main/java/com/kbeanie/imagechooser/api/ChooserType.java
+++ image-chooser-library/src/main/java/com/kbeanie/imagechooser/api/ChooserType.java
@@ -19,11 +19,15 @@ public interface ChooserType {
     int REQUEST_PICK_PICTURE = 291;
     
     int REQUEST_CAPTURE_PICTURE = 294;
-    
+
+    int REQUEST_CAPTURE_OR_PICK_PICTURE = 297;
+
     int REQUEST_CAPTURE_VIDEO = 292;
-    
+
     int REQUEST_PICK_VIDEO = 295;
-    
+
+    int REQUEST_CAPTURE_OR_PICK_VIDEO = 298;
+
     int REQUEST_PICK_PICTURE_OR_VIDEO = 300;
 
     int REQUEST_PICK_FILE = 500;
